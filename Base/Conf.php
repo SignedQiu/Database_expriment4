0f654b49b010de7d2741e27bd07995df6b1f5025
signedqiu <2470508613@qq.com>
2021-06-14 16:43:40 +0800

    first commit

---
+++ Base/Conf.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    //数据库配置
+    'db_host'   =>  "127.0.0.1",
+    'db_user'   =>  "signedqiu",
+    'db_pwd'    =>  "247050",
+    'db_name'   =>  "book",
+    'db_port'   =>  "3306",
+    'charset'   =>  "utf8",
+
+    //默认路由配置
+    'default_plantform'   => "Common",
+    'default_controller'  => "Login",
+    'default_action'      => "index"
+);
